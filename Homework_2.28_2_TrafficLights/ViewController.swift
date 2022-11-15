af949b2074351d25c846d4e53e5b8798d938f339
Ravil <ravilgab@yahoo.com>
2022-11-16 00:33:56 +0300

    1. Small codestyle changes. 2. Added reset button.

--- Homework_2.28_2_TrafficLights/ViewController.swift
+++ Homework_2.28_2_TrafficLights/ViewController.swift
@@ -17,47 +17,66 @@ class ViewController: UIViewController {
 
     let lightsOn: Float = 1
     let lightsOff: Float = 0.2
+    let buttonCornerRadius: CGFloat = 9
     var currentColor: Lights = .green
     
-    @IBOutlet weak var redLabel: UIView!
-    @IBOutlet weak var yellowLabel: UIView!
-    @IBOutlet weak var greenLabel: UIView!
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
     
     @IBOutlet weak var startButtonOutlet: UIButton!
+    @IBOutlet weak var resetButtonOutlet: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redLabel.layer.opacity = lightsOff
-        yellowLabel.layer.opacity = lightsOff
-        greenLabel.layer.opacity = lightsOff
+        redLightView.layer.opacity = lightsOff
+        yellowLightView.layer.opacity = lightsOff
+        greenLightView.layer.opacity = lightsOff
         
-        startButtonOutlet.layer.cornerRadius = 9
+        startButtonOutlet.layer.cornerRadius = buttonCornerRadius
+        resetButtonOutlet.layer.cornerRadius = buttonCornerRadius
+        
+        resetButtonOutlet.layer.backgroundColor = UIColor.systemGray.cgColor
     }
     
     override func viewDidLayoutSubviews() {
-        redLabel.layer.cornerRadius = redLabel.layer.bounds.width / 2
-        yellowLabel.layer.cornerRadius = yellowLabel.layer.bounds.width / 2
-        greenLabel.layer.cornerRadius = greenLabel.layer.bounds.width / 2
+        redLightView.layer.cornerRadius = redLightView.layer.bounds.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.layer.bounds.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.layer.bounds.width / 2
     }
 
     @IBAction func startButtonPressed() {
         startButtonOutlet.setTitle("Next", for: .normal)
         
+        resetButtonOutlet.layer.backgroundColor = #colorLiteral(red: 0, green: 0.300072529, blue: 0.6560639881, alpha: 1)
+        
         switch currentColor {
         case .red:
-            redLabel.layer.opacity = lightsOff
-            yellowLabel.layer.opacity = lightsOn
+            redLightView.layer.opacity = lightsOff
+            yellowLightView.layer.opacity = lightsOn
             currentColor = .yellow
         case .yellow:
-            yellowLabel.layer.opacity = lightsOff
-            greenLabel.layer.opacity = lightsOn
+            yellowLightView.layer.opacity = lightsOff
+            greenLightView.layer.opacity = lightsOn
             currentColor = .green
         case .green:
-            greenLabel.layer.opacity = lightsOff
-            redLabel.layer.opacity = lightsOn
+            greenLightView.layer.opacity = lightsOff
+            redLightView.layer.opacity = lightsOn
             currentColor = .red
         }
+    }
+    
+    @IBAction func resetButtonPressed() {
+        redLightView.layer.opacity = lightsOff
+        yellowLightView.layer.opacity = lightsOff
+        greenLightView.layer.opacity = lightsOff
+        
+        startButtonOutlet.setTitle("Start", for: .normal)
+    
+        resetButtonOutlet.layer.backgroundColor = UIColor.systemGray.cgColor
+        
+        currentColor = .green
     }
     
 }
