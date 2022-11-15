037644ba6fc6e50318b7f7a093dfdfc9193bd0da
Ravil <ravilgab@yahoo.com>
2022-11-15 23:30:28 +0300

    Made changes to interface with code. No logic yet.

--- Homework_2.28_2_TrafficLights/ViewController.swift
+++ Homework_2.28_2_TrafficLights/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let accentColor = UIColor.white.cgColor
+    let lightsOn: Float = 1
+    let lightsOff: Float = 0.2
+    
+    @IBOutlet weak var redLabel: UIView!
+    @IBOutlet weak var yellowLabel: UIView!
+    @IBOutlet weak var greenLabel: UIView!
+    
+    @IBOutlet weak var startButtonOutlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLabel.layer.opacity = lightsOff
+        yellowLabel.layer.opacity = lightsOff
+        greenLabel.layer.opacity = lightsOff
+        
+        startButtonOutlet.layer.cornerRadius = 9
+
+    }
+    
+    override func viewDidLayoutSubviews() {
+        redLabel.layer.cornerRadius = redLabel.layer.bounds.width / 2
+        yellowLabel.layer.cornerRadius = yellowLabel.layer.bounds.width / 2
+        greenLabel.layer.cornerRadius = greenLabel.layer.bounds.width / 2
+        
+        
     }
 
-
+    @IBAction func startButtonPressed() {
+        startButtonOutlet.setTitle("Next", for: .normal)
+    }
+    
 }
 
